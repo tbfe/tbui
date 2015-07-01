11dfdb9b21da9a1976b2c9c831a64ddeeda908f8
tb_fe <tb_fe@baidu.com>
2015-07-01 12:48:55 +0800

    add dependency

--- deploy/setting/variable.js
+++ deploy/setting/variable.js
@@ -1,1 +1,1 @@
-window.color_varible = {"@white-color":"#fff","@black-color":"#000","@gray-color":"#666","@gray-darker-color":"#333","@gray-lighter-color":"#999","@red-color":"#f74d4a","@orange-color":"#f8984a","@blue-color":"#2d64b3","@visited-color":"#653096"}
+window.color_varible = {}
