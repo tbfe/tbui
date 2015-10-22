0da1a30d56d653bc00353d86e4698a220ccee1b5
yuanzhen <yuanzhen@baidu.com>
2015-10-22 16:52:34 +0800

    add dependency

--- deploy/setting/icon.js
+++ deploy/setting/icon.js
@@ -1,1 +1,1 @@
-window.icon_varible = 'icon-loading-32.gif,icon-loading-64.gif,tbean.png,vip1-16.png,vip1-18.png,vip1-gray-16.png,vip1-gray-18.png,vip2-16.png,vip2-18.png,vip2-gray-16.png,vip2-gray-18.png'
+window.icon_varible = 'icon-loading-32.gif,icon-loading-64.gif,tbean.png,vip1-16.png,vip1-18.png,vip1-gray-16.png,vip1-gray-18.png,vip2-16.png,vip2-18.png,vip2-gray-16.png,vip2-gray-18.png,vip3-16.png,vip3-26.png,vip3-gray-16.png,vip3-gray-26.png'
